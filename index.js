9c78ed0e14eb0bcfc6d2b26824a9f290ee3899c2
JcerelusDev <54859793+JcerelusDev@users.noreply.github.com>
2021-10-06 14:25:41 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+import Ajaxy  from "./ajaxy.js";
+import routing  from "./route.js";
+import {servePage} from "./route.js";
+
+import menuComponent from "./menu-component.js";
+import homeComponent from "./home-component.js";
+import aboutHeaderComponent from "./about-header-component.js";
+import aboutComponent from "./about-component.js";
+import projectsComponent from "./projects-component.js";
+import contactComponent from "./contact-component.js";
+
+const renderMenu = function(){
+ let menu_component =  document.createElement("menu-component");
+  document.body.appendChild(menu_component);
+
+};
+
+let View = document.querySelector("#root");
+export default View;
+
+ 
+ var home_component =  document.createElement("home-component");
+
+ export{home_component};
+
+
+window.addEventListener( "load", function(){
+    renderMenu();
+ View.appendChild(home_component);
+ 
+  if(location.hash.includes("#/")){
+    View.appendChild(home_component);
+    
+  }
+    
+    
+});
+
+
+
+
+
