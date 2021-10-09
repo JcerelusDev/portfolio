b8c7c5270e013d3a022602c6f4db834cb1750d25
JcerelusDev <54859793+JcerelusDev@users.noreply.github.com>
2021-10-08 22:24:48 -0500

    Update menu-component.js

--- js/menu-component.js
+++ js/menu-component.js
@@ -26,7 +26,7 @@ template.innerHTML =`
 
 <div class="footer">
 <div class="logolink1"><a  href="https://www.facebook.com/jcerelus/" target="_blank"><img width="20" src="img/facebook.svg" class="linkIcon"></a></div>
-<div class="logolink2"><a class="logolink2" href="https://www.instagram.com/tv/CDnQjGdFzoi/?utm_medium=copy_link" target="_blank"><img width="20" src="img/instagram.svg" class="linkIcon"></a></div>
+<div class="logolink2"><a href="https://www.instagram.com/tv/CDnQjGdFzoi/?utm_medium=copy_link" target="_blank"><img width="20" src="img/instagram.svg" class="linkIcon"></a></div>
 <p class="copy">&copy;jcerelusdev 2021 All Rights Reserved.</p>
 </div>
 
