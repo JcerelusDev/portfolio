9ab953cc60e808fcf70d51832f6cb235f5e209e2
JcerelusDev <54859793+JcerelusDev@users.noreply.github.com>
2022-07-13 10:51:56 -0500

    Update contact-component.js

--- js/contact-component.js
+++ js/contact-component.js
@@ -104,7 +104,7 @@ margin-bottom: 120px;
 
 <div class="contactcontainer">
 <div id="contactcontent">
-<form  action="https://mail.google.com/mail/mu/mp/857/#co" method="POST" enctype="text/plain" target="_blank">
+<form  action="https://formsubmit.co/johnjustme8@gmail.com " method="POST" enctype="text/plain" target="_blank">
 <fieldset>
 <legend class="title">&Rarr; Contact Me &Larr;</legend>
 <br
